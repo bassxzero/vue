3f1395eb96b51882674a4a0e5e8ec8fa84553ebb
Tyler Scott <bassxzero@yahoo.com>
2021-03-09 13:14:59 -0500

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+//require('./bootstrap');
+
+import Vue from 'vue'
+
+import Test from './test'
+
+new Vue({
+    el: '#root',
+    render: h => h(Test),
+})
---
+++ resources/js/test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        I changed the DOM.
+    </div>
+</template>
+
+<script>
+
+export default{
+    name: 'Test',
+    props: {
+
+    },
+}
+</script>
